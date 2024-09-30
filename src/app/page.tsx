42c539798ca0d2a5b08dd99e72d57f3a7a28dcec
Pp <gamerer16@gmail.com>
2024-09-30 15:58:43 +0700

    done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,10 @@
 "use client";
 
+import PostOwnner from "@/components/PostOwnner";
+import Comment from "@/components/Comment";
+import { comments } from "@/libs/comments";
+import { CommentProps } from "@/libs/types";
+
 export default function Home() {
   return (
     <div
@@ -12,82 +17,20 @@ export default function Home() {
         className="mx-auto p-3 rounded rounded-3 shadow-sm"
       >
         {/* Post Owner Example*/}
-        <div className="vstack gap-3">
-          <div className="d-flex align-items-center gap-3">
-            <img
-              src="/profileImages/handsome.jpg"
-              width="48"
-              height="48"
-              className="rounded-circle"
-              style={{ objectFit: "cover" }}
-            />
-            <span className="fw-semibold fs-5 text-white">
-              Chayanin Suatap 650610560
-            </span>
-          </div>
 
-          <span className="text-white">
-            Quiz ง่ายจังเลยครับ ขอยาก ๆ กว่านี้ได้ไหม #261207
-          </span>
-
-          <div className="d-flex align-items-center gap-1">
-            <img src="/like.svg" width={20}></img>
-            <span style={{ color: "#B0B3B8" }}>100 คน</span>
-          </div>
-          <hr className="m-0 border" />
-        </div>
+        <PostOwnner ImagePath={"/profileImages/pimolnat.png"} 
+        username={"Pimolnat Kaewboot 660610779"} 
+        postTitle="Quiz ง่ายจังเลยครับ ขอยาก ๆ กว่านี้ได้ไหม #261207" 
+        likes={100} 
+        />
 
         {/* Comment Example */}
-        <div className="d-flex gap-2 my-2">
-          <img
-            src="/profileImages/lisa.jpg"
-            width="48"
-            height="48"
-            className="rounded-circle"
-            style={{ objectFit: "cover" }}
-          />
-          <div
-            className="rounded rounded-3 p-2"
-            style={{ backgroundColor: "#3A3B3C" }}
-          >
-            <span className="fw-semibold" style={{ color: "#E4E6EB" }}>
-              Lisa
-            </span>
-            <br />
-            <span style={{ color: "#E4E6EB" }}>จริงค่า</span>
-            <div className="d-flex align-items-center gap-1">
-              <img src="/like.svg" width={20}></img>
-              <span style={{ color: "#B0B3B8" }}>999 คน</span>
-            </div>
-          </div>
-        </div>
+      
 
         {/* Reply Example */}
-        <div className="d-flex gap-2 my-2 ps-5">
-          <img
-            src="/profileImages/puppy.jpg"
-            width="48"
-            height="48"
-            className="rounded-circle"
-            style={{ objectFit: "cover" }}
-          />
-          <div
-            className="rounded rounded-3 p-2"
-            style={{ backgroundColor: "#3A3B3C" }}
-          >
-            <span className="fw-semibold" style={{ color: "#E4E6EB" }}>
-              หมาน้อย
-            </span>
-            <br />
-            <span style={{ color: "#E4E6EB" }}>จริงค้าบบบบบบบบ</span>
-            <div className="d-flex align-items-center gap-1">
-              <img src="/like.svg" width={20}></img>
-              <span style={{ color: "#B0B3B8" }}>2 คน</span>
-            </div>
-          </div>
-        </div>
-
+       
         {/* map-loop render Comment component here */}
+        {comments.map((comment: CommentProps) => (<Comment key={comment.username} {...comment}/>))}
       </div>
     </div>
   );
--- src/libs/types.ts
+++ src/libs/types.ts
@@ -1,5 +1,27 @@
 // You can define interfaces for all React components here.
 // Do not forget to export properly.
 
-interface ReplyProps {}
+interface ReplyProps {
+    username: string;
+    userImagePath: string;
+    replyText: string;
+    likeNum: number;
+}
 export type { ReplyProps };
+
+interface PostOwnerProps {
+    ImagePath: string;
+    username: string;
+    postTitle:string;
+    likes:number;
+}
+export type { PostOwnerProps };
+
+interface CommentProps {
+    userImagePath: string;
+    username: string;
+    commentText:string;
+    likeNum:number;
+    replies:ReplyProps[] | any;
+}
+export type { CommentProps };
